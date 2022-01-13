89e410bd68d2bb84de6cc1afa5ed7393efe4a032
shivashankarcodez <42712441+shivashankarcodez@users.noreply.github.com>
2022-01-13 16:15:33 +0530

    Update Pattern3.java

--- prgms/number/Pattern3.java
+++ prgms/number/Pattern3.java
@@ -11,7 +11,7 @@ public class Pattern3 {
 		int n = sca.nextInt();
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
-				if (i>=0&&j==0||i==j&&j>=n/2||i+j==n-1&&i<3*(n/4)) {
+				if (j==n-1 || i ==0 || i==n-1 || i==n/2) {
 					System.out.print("*");
 				} else {
 					System.out.print(" ");
